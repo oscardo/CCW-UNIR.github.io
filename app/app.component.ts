f01fddff3f2a54fefbf4c62218d6056874a920f4
Oscar Eduardo Ortiz Pinzon <oscardo2000@hotmail.com>
2018-07-22 17:47:48 -0500

    primera version de la página web

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Oscar Eduardo Ortiz Pinzón';
+  email = 'oscardo.ortiz@gmail.com';
+  website = 'https://oscardo.github.io';
+  hobbies = ['Leer', 'Correr', 'Historia'];
+
+}
